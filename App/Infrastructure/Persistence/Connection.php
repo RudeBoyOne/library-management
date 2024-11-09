1708aac71409ffee5b9e9e3deb503b5e504aad67
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2024-11-09 23:21:02 +0000

    DOCS: added Connection class documentation

--- App/Infrastructure/Persistence/Connection.php
+++ App/Infrastructure/Persistence/Connection.php
@@ -3,12 +3,38 @@ namespace App\Library\Infrastructure\Persistence;
 
 use PDO;
 
+/**
+ * Singleton class to manage database connection.
+ * 
+ * This class uses the Singleton pattern to ensure that only one instance
+ * of the database connection is created and reused during the application's lifecycle.
+ * The connection is established using PDO with a database path defined by an environment variable. 
+ */
 class Connection
 {
+    /**
+     * Single instance of the connection.
+     * @var self|null
+     */
     private static $instance = null;
+    /**
+     * Database path.
+     * @var string 
+     */
     private $path;
+    /**
+     * PDO instance for the database connection.
+     * @var PDO 
+     */
     private PDO $connection;
 
+    /**
+     * Class constructor.
+     * 
+     * The constructor is private to prevent creating multiple instances of the class.
+     * It initializes the database connection using the path specified in the DB_PATH environment variable.
+     * @throws \PDOException If the database connection fails. 
+     */
     public function __construct()
     {
         $this->path = getenv('DB_PATH');
@@ -16,6 +42,10 @@ class Connection
         $this->connection = new PDO($dsn, null, null, [PDO::ATTR_PERSISTENT => true]);
     }
 
+    /**
+     * Gets the single instance of the connection.
+     * @return PDO The PDO instance for the database connection.
+     */
     public static function getInstance()
     {
         if (self::$instance === null) {
